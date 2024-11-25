651e1b83a634668ce6487931364b0ae0f1cc55a0
Yousseff2 <93355191+Yousseff2@users.noreply.github.com>
2024-11-26 01:20:22 +0200

    Delete home directory

--- home/mywork/work.js
+++
@@ -1,7 +0,0 @@
-function resizeIframe() {
-  const iframe = document.getElementById("iframe");
-  iframe.style.height =
-    iframe.contentWindow.document.documentElement.scrollHeight + "px";
-}
-window.addEventListener("load", resizeIframe);
-window.addEventListener("resize", resizeIframe);
